9a2177280fb0efe58166402b7f5bf64fa0af92fa
ReSolDominus <pavlenkokirya02@gmail.com>
2024-11-23 14:39:54 +0200

    lib Compile

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Cours_5\SmartSurveillance\SmartSurveillance\SmartSurveillance\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
