c2938dbe03465328d5280d2c8c466ee41cc7bad7
ReSolDominus <mariamihajlova31@gmail.com>
2024-11-29 12:38:30 +0200

    Update urls.py

--- SmartSurveillance/urls.py
+++ SmartSurveillance/urls.py
@@ -6,6 +6,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('video_feed/', views.video_feed, name='video_feed'),
+    path('video_feed/<int:camera_id>/', views.video_feed, name='video_feed'),
     path('', views.index, name='index'),
 ]
