f360334adc4cd07e5bca07a430fbe724e42bb9a6
ReSolDominus <pavlenkokirya02@gmail.com>
2024-11-29 12:31:14 +0200

    Update views.py

--- SmartSurveillance/views.py
+++ SmartSurveillance/views.py
@@ -2,12 +2,20 @@ import cv2
 from django.http import StreamingHttpResponse
 from django.shortcuts import render
 
-def generate_frames():
-    # Відкриваємо камеру
-    camera = cv2.VideoCapture(0)
+# Глобальна змінна для збереження відкритої камери
+camera = None
+
+def generate_frames(camera_index):
+    global camera
+
+    # Закриваємо попередню камеру, якщо вона відкрита
+    if camera is not None:
+        camera.release()
+
+    # Відкриваємо нову камеру
+    camera = cv2.VideoCapture(camera_index)
     if not camera.isOpened():
-        raise Exception("Не вдалося відкрити камеру")
-      
+        raise Exception(f"Не вдалося відкрити камеру {camera_index}")
 
     while True:
         success, frame = camera.read()
@@ -23,8 +31,12 @@ def generate_frames():
             # Невелика затримка для стабільності
             cv2.waitKey(1)
 
-def video_feed(request):
-    return StreamingHttpResponse(generate_frames(), content_type='multipart/x-mixed-replace; boundary=frame')
+def video_feed(request, camera_id=0):
+    print(f"Отриманий camera_id: {camera_id}")
+    return StreamingHttpResponse(
+        generate_frames(camera_id), 
+        content_type='multipart/x-mixed-replace; boundary=frame'
+    )
 
 def index(request):
     return render(request, 'index.html')
