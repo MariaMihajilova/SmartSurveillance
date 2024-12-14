fc97ec4f154a48e6c6b200e91325460fda80bd9e
ReSolDominus <mariamihajlova31@gmail.com>
2024-12-14 12:49:30 +0200

    Update urls

--- SmartSurveillance/urls.py
+++ SmartSurveillance/urls.py
@@ -4,8 +4,10 @@
 
 from django.urls import path
 from . import views
+from .views import save_screenshot
 
 urlpatterns = [
     path('video_feed/<int:camera_id>/', views.video_feed, name='video_feed'),
     path('', views.index, name='index'),
+    path('save-screenshot/', save_screenshot, name='save_screenshot'),
 ]
